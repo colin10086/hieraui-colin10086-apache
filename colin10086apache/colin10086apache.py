6fe1456873144b76ad9cec34a814039a551e5a5f
colin10086 <colin@Colin-Barbers-MacBook-Air.local>
2015-06-18 15:19:30 -0700

    d

--- colin10086apache/colin10086apache.py
+++ colin10086apache/colin10086apache.py
@@ -3,14 +3,6 @@ from app.form import Form
 class colin10086apache(Form):
 
  def validate(_self, form_data):
-  valid = True
-  if ‘ghost’ not in form_data or \
-   ‘docroot’ not in form_data or \
-   ‘hostname’ not in form_data or \
-   ‘source_repo’ not in form_data:
-   valid = False
-  if valid is False:
-   return “Missing info!”
   return True
 
  def process(_self, form_data):
